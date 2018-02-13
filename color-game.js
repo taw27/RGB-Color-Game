bea593376246f64fae03562c4d6306e2344a0176
Tawshif Alam <tawshif27@gmail.com>
2018-02-12 18:36:50 -0800

    Change tile colors to random colors

--- color-game.js
+++ color-game.js
@@ -1,17 +1,29 @@
-document.getElementById("new-colors-button").addEventListener("click",newColorClickedChangeHead);
+var correctTile;
+document.getElementById("new-colors-button").addEventListener("click",newColorClicked);
 //function for new colors
-function newColorClickedChangeHead(){
-    var rgbArray= new Array(3);
-    rgbArray= randomRGB();
-    var rgbString="("+rgbArray[0]+", "+rgbArray[1]+", "+rgbArray[2]+")";
-    document.querySelector("h1 span").textContent=rgbString;
+function newColorClicked(){
+    var randomRgb=randomRgbString();
+    var tileHard=6;
+    correctTile= Math.floor(Math.random()*5)+1;
+    //changes header
+    document.querySelector("h1 span").textContent=randomRgb;
 
+    for(var i=1; i<=tileHard;i++){
+        if(i!==correctTile){
+            document.getElementById("tile-"+i).style.background="rgb"+randomRgbString();
+        }
+        else{
+            document.getElementById("tile-"+correctTile).style.background="rgb"+randomRgb;
 
-    
+        }
+    }
+
 }
-// function that returns random rgb array
-function randomRGB(){
+//Function to for
+// function that returns random rgb String
+function randomRgbString(){
     var rgbArray=[Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255)];
+    var rgbString="("+rgbArray[0]+", "+rgbArray[1]+", "+rgbArray[2]+")";
 
-    return rgbArray;
+    return rgbString;
 }
